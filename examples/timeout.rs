cb3d2d4849f273d1805157b44473af4b425add7e
Spencer C. Imbleau <spencer@imbleau.com>
2023-06-04 10:31:49 -0400

    split examples

--- examples/timeout.rs
+++ examples/timeout.rs
@@ -8,25 +8,26 @@ async fn long_task() -> u32 {
     5
 }
 
-fn my_system(mut task_executor: AsyncTaskRunner<u32>) {
+fn my_impatient_system(mut task_executor: AsyncTaskRunner<Result<u32, TimeoutError>>) {
     match task_executor.poll() {
         AsnycTaskStatus::Idle => {
-            let new_task = AsyncTask::new(long_task());
+            let new_task = AsyncTimeoutTask::new(Duration::from_millis(500), long_task());
             task_executor.begin(new_task);
             println!("Started!");
         }
         AsnycTaskStatus::Pending => {
             // Waiting...
         }
-        AsnycTaskStatus::Finished(v) => {
-            println!("Received {v}");
-        }
+        AsnycTaskStatus::Finished(v) => match v {
+            Ok(_) => panic!("This should always time-out"),
+            Err(_) => println!("A timeout happened in `my_impatient_system`!"),
+        },
     }
 }
 
 pub fn main() {
     App::new()
         .add_plugins(MinimalPlugins)
-        .add_system(my_system)
+        .add_system(my_impatient_system)
         .run();
 }
