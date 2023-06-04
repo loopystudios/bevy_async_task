8851112d42899d8118675a493c93da815dcd84ba
Spencer C. Imbleau <spencer@imbleau.com>
2023-06-04 00:33:41 -0400

    added impatient system

--- examples/systems.rs
+++ examples/systems.rs
@@ -1,6 +1,6 @@
-use async_std::task::sleep;
+use async_std::{future::TimeoutError, task::sleep};
 use bevy::prelude::*;
-use bevy_async_task::{AsnycTaskStatus, AsyncTask, AsyncTaskRunner};
+use bevy_async_task::{AsnycTaskStatus, AsyncTask, AsyncTaskRunner, AsyncTimeoutTask};
 use std::time::Duration;
 
 async fn long_task() -> u32 {
@@ -24,9 +24,27 @@ fn my_system(mut task_executor: AsyncTaskRunner<u32>) {
     }
 }
 
+fn my_impatient_system(mut task_executor: AsyncTaskRunner<Result<u32, TimeoutError>>) {
+    match task_executor.poll() {
+        AsnycTaskStatus::Idle => {
+            let new_task = AsyncTimeoutTask::new(Duration::from_millis(500), long_task());
+            task_executor.begin(new_task);
+            println!("Started!");
+        }
+        AsnycTaskStatus::Pending => {
+            // Waiting...
+        }
+        AsnycTaskStatus::Finished(v) => match v {
+            Ok(_) => panic!("This should always time-out"),
+            Err(_) => println!("A timeout happened in `my_impatient_system`!"),
+        },
+    }
+}
+
 pub fn main() {
     App::new()
         .add_plugins(MinimalPlugins)
         .add_system(my_system)
+        .add_system(my_impatient_system)
         .run();
 }
