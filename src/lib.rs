4af6246db7ddef42658fc1a764ae6845da7f6796
Spencer C. Imbleau <spencer@imbleau.com>
2023-06-09 14:07:36 -0400

    Separated wasm/native AsyncTask

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,17 @@ use bevy::ecs::world::unsafe_world_cell::UnsafeWorldCell;
 use bevy::prelude::*;
 use bevy::tasks::AsyncComputeTaskPool;
 use bevy::utils::synccell::SyncCell;
-use std::{future::Future, pin::Pin};
 use tokio::sync::oneshot;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod native;
+#[cfg(not(target_arch = "wasm32"))]
+pub use native::AsyncTask;
+
+#[cfg(target_arch = "wasm32")]
+mod wasm;
+#[cfg(target_arch = "wasm32")]
+pub use wasm::AsyncTask;
 
 /// A task runner which executes [`AsyncTask`]s in the background.
 pub struct AsyncTaskRunner<'s, T>(pub(crate) &'s mut Option<AsyncReceiver<T>>);
@@ -100,64 +109,6 @@ pub enum AsyncTaskStatus<T> {
     Finished(T),
 }
 
-/// A task than may be ran by an [`AsyncTaskRunner`], or broken into parts.
-pub struct AsyncTask<T> {
-    fut: Pin<Box<dyn Future<Output = ()> + Send + 'static>>,
-    receiver: AsyncReceiver<T>,
-}
-
-impl<T> AsyncTask<T> {
-    pub fn new<F>(fut: F) -> Self
-    where
-        F: Future<Output = T> + Send + 'static,
-        F::Output: Send + 'static,
-    {
-        let (tx, rx) = oneshot::channel();
-        let new_fut = async move {
-            let result = fut.await;
-            _ = tx.send(result);
-        };
-        let fut = Box::pin(new_fut);
-        let receiver = AsyncReceiver {
-            received: false,
-            buffer: rx,
-        };
-        Self { fut, receiver }
-    }
-
-    /// Block awaiting the task result. Can only be used outside of async contexts.
-    ///
-    /// # Panics
-    /// Panics if called within an async context.
-    pub fn blocking_recv(self) -> T {
-        let (fut, rx) = self.into_parts();
-        futures::executor::block_on(fut);
-        rx.buffer.blocking_recv().unwrap()
-    }
-
-    /// Break apart the task into a runnable future and the receiver. The receiver is used to catch the output when the runnable is polled.
-    #[allow(clippy::type_complexity)]
-    #[must_use]
-    pub fn into_parts(
-        self,
-    ) -> (
-        Pin<Box<dyn Future<Output = ()> + Send + 'static>>,
-        AsyncReceiver<T>,
-    ) {
-        (self.fut, self.receiver)
-    }
-}
-
-impl<T, Fnc> From<Fnc> for AsyncTask<T>
-where
-    Fnc: Future<Output = T> + Send + 'static,
-    Fnc::Output: Send + 'static,
-{
-    fn from(value: Fnc) -> Self {
-        AsyncTask::new(value)
-    }
-}
-
 pub struct AsyncReceiver<T> {
     received: bool,
     buffer: oneshot::Receiver<T>,
@@ -178,63 +129,5 @@ impl<T> AsyncReceiver<T> {
             Err(oneshot::error::TryRecvError::Empty) => None,
             _ => panic!("the sender was dropped without sending"),
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use futures::{pin_mut, FutureExt};
-    use futures_timer::Delay;
-    use std::time::Duration;
-    use tokio::select;
-
-    #[tokio::test]
-    async fn test_oneshot() {
-        let (tx, rx) = oneshot::channel();
-
-        tokio::spawn(async move {
-            if tx.send(3).is_err() {
-                panic!("the receiver dropped");
-            }
-        });
-
-        match rx.await {
-            Ok(v) => assert_eq!(3, v),
-            Err(_) => panic!("the sender dropped"),
-        }
-    }
-
-    #[test]
-    fn test_blocking_recv() {
-        let task = AsyncTask::new(async move { 5 });
-        assert_eq!(5, task.blocking_recv());
-    }
-
-    #[tokio::test]
-    async fn test_try_recv() {
-        let task = AsyncTask::new(async move { 5 });
-        let (fut, mut rx) = task.into_parts();
-
-        assert_eq!(None, rx.try_recv());
-
-        // Spawn
-        tokio::spawn(fut);
-
-        // Wait for response
-        let fetch = Delay::new(Duration::from_millis(1)).fuse();
-        let timeout = Delay::new(Duration::from_millis(100)).fuse();
-        pin_mut!(timeout, fetch);
-        select! {
-            _ = &mut fetch => {
-                if let Some(v) = rx.try_recv() {
-                    assert_eq!(5, v);
-                } else {
-                    // Reset the clock
-                    fetch.set(Delay::new(Duration::from_millis(1)).fuse());
-                }
-            }
-            _ = timeout => panic!("timeout")
-        };
     }
 }
---
+++ src/wasm/mod.rs
@@ -0,0 +1,87 @@
+use crate::AsyncReceiver;
+use std::{future::Future, pin::Pin};
+use tokio::sync::oneshot;
+
+/// A task than may be ran by an [`AsyncTaskRunner`], or broken into parts.
+pub struct AsyncTask<T> {
+    fut: Pin<Box<dyn Future<Output = ()> + 'static>>,
+    receiver: AsyncReceiver<T>,
+}
+
+impl<T> AsyncTask<T> {
+    pub fn new<F>(fut: F) -> Self
+    where
+        F: Future<Output = T> + 'static,
+        F::Output: Send + 'static,
+    {
+        let (tx, rx) = oneshot::channel();
+        let new_fut = async move {
+            let result = fut.await;
+            _ = tx.send(result);
+        };
+        let fut = Box::pin(new_fut);
+        let receiver = AsyncReceiver {
+            received: false,
+            buffer: rx,
+        };
+        Self { fut, receiver }
+    }
+
+    /// Block awaiting the task result. Can only be used outside of async contexts.
+    ///
+    /// # Panics
+    /// Panics if called within an async context.
+    pub fn blocking_recv(self) -> T {
+        let (fut, rx) = self.into_parts();
+        futures::executor::block_on(fut);
+        rx.buffer.blocking_recv().unwrap()
+    }
+
+    /// Break apart the task into a runnable future and the receiver. The receiver is used to catch the output when the runnable is polled.
+    #[allow(clippy::type_complexity)]
+    #[must_use]
+    pub fn into_parts(
+        self,
+    ) -> (
+        Pin<Box<dyn Future<Output = ()> + 'static>>,
+        AsyncReceiver<T>,
+    ) {
+        (self.fut, self.receiver)
+    }
+}
+
+impl<T, Fnc> From<Fnc> for AsyncTask<T>
+where
+    Fnc: Future<Output = T> + 'static,
+    Fnc::Output: Send + 'static,
+{
+    fn from(value: Fnc) -> Self {
+        AsyncTask::new(value)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    #[wasm_bindgen_test]
+    fn test_blocking_recv() {
+        let task = AsyncTask::new(async move { 5 });
+        assert_eq!(5, task.blocking_recv());
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_try_recv() {
+        let task = AsyncTask::new(async move { 5 });
+        let (fut, mut rx) = task.into_parts();
+
+        assert_eq!(None, rx.try_recv());
+
+        // Spawn
+        wasm_bindgen_futures::spawn_local(async move {
+            fut.await;
+            assert_eq!(Some(5), rx.try_recv());
+        });
+    }
+}
